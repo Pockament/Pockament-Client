eaf1653a9698059244df15c14169db1ebd4f8104
Yuki Yuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-12-14 01:12:12 +0900

    add comment

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,14 @@
 import { FC } from "react";
 import style from "../style/footer.module.scss";
+
 const Footer: FC = () => {
   return <div></div>;
 };
 
 export default Footer;
+/* 作る機能
+ * ============
+ * 残りファイル数 8/152 みたいに
+ * 通信速度
+ * ============
+ */
